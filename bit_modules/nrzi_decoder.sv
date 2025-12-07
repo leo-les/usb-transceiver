9e28317c584d4a2b2beaee4ca169e946014fb6af
jiayil-1 <liu3988@purdue.edu>
2025-12-07 14:39:57 -0500

    Modify nrzi_decoder for decoding functionality

--- bit_modules/nrzi_decoder.sv
+++ bit_modules/nrzi_decoder.sv
@@ -1,30 +1,34 @@
 module nrzi_decoder(
     input logic clk, nRST,
-    input logic start_encoding, // enable to start encoding
+    input logic start_decoding, // enable to start encoding
     input logic curr_bit,
     output logic decoded_bit
 );
 
-logic prev_decoded_bit;
+logic prev_encoded_bit;
 
 always_comb begin
-    if (start_encoding == 0) begin
+    if (start_decoding == 0) begin
         // decoded bit should be?
-        decoded_bit = 0;
+        decoded_bit = 1; // Idle state
     end else begin
-        // 
-        decoded_bit = 
+        // if current bit = 1, 
+        if(curr_bit == prev_encoded_bit) 
+            decoded_bit = 1;
+        else
+            decoded_bit = 0;
     end
 end
 
 // trigger block when a rising edge of clk or rst is detected
-always_ff @(posedge clk or posedge rst) begin
+    always_ff @(posedge clk or negedge nRST) begin
     if (!nRST) begin
         // if reset, set previous encoded bit to 0
         prev_encoded_bit <= 0;
+        decoded_bit <= 1; // Idle state
     end else begin
         // assign encoded bit to the 1 bit register prev_encoded_bit to be used in next call
-        prev_decoded_bit <= decoded_bit;
+        prev_encoded_bit <= curr_bit;
     end
 end
 
