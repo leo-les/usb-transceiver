bbf27b3d73e3b6609ecf7f0fdca4bdb490abc4b9
jiayil-1 <liu3988@purdue.edu>
2025-12-10 16:54:59 -0500

    Replace start_encoding with enable for NRZI encoding

--- src/bit_modules/nrzi_encoder.sv
+++ src/bit_modules/nrzi_encoder.sv
@@ -1,14 +1,14 @@
 `default_nettype none
 module nrzi_encoder(
     input logic clk, nRST,
-    input logic start_encoding, // enable to start encoding
+    input logic enable, // enable to start encoding
     input logic curr_bit,
     output logic encoded_bit
 );
 logic prev_encoded_bit, next_encoded_bit;
 always_comb begin
-    if (start_encoding == 0) begin
-        next_encoded_bit = 1;
+    if (enable == 0) begin
+        next_encoded_bit = encoded_bit;
     end else begin
         next_encoded_bit = (curr_bit == 0) ? ~prev_encoded_bit : prev_encoded_bit;
     end
@@ -16,14 +16,14 @@ end
     always_ff @(posedge clk or negedge nRST) begin
         if (!nRST) begin
             encoded_bit <= 1; // Idle state 
-        end else begin
+        end else if(enable) begin
             encoded_bit <= next_encoded_bit;
         end
     end
     always_ff @(negedge clk or negedge nRST) begin
         if (!nRST) begin
             prev_encoded_bit <= 1;
-        end else begin
+        end else if(enable) begin
             prev_encoded_bit <= encoded_bit;
         end
     end
